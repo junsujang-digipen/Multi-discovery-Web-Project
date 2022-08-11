77fa79b1f4394f66bd3d369b422d0fcf5191e2c6
junsujang-digipen <65064387+junsujang-digipen@users.noreply.github.com>
2022-08-11 20:20:16 +0900

    web start

--- Codes/main.py
+++ Codes/main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+ 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return 'Hello, World!'
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/",  methods=['GET', 'POST'])
+def index(): 
+    if request.method == 'GET':
+        name = request.args.get("keyword")
+        print(request.form)
+        
+    return render_template('index.html', name = name)
+
+@app.route('/result/')
+def search():
+    if request.method == 'GET':
+        name = request.args.get("keyword")
+    return name
+
+if __name__ == "__main__":
+    app.run()
